4db6ff2c1169ea2f2fd779ad5504a83b433dfc63
Janniel Tan <tanjanniel@gmail.com>
2025-03-06 14:55:29 -0800

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Janniel Tan on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
